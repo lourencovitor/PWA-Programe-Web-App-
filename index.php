624bc3839da4438e7ba7dc5142fe5c28524add02
VITOR LOURENCO DA SILVA <vitor.lsilva14@senacsp.edu.br>
2019-10-04 20:32:17 -0300

    First commit project PWA

---
+++ index.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="manifest" href="./manifest.json">
+    <link rel="shortcut icon" href="img/icons/icon-72x72.png">
+    <title>Galeria de games</title>
+</head>
+
+<body class="bg-light">
+    <div class="div-pai">
+        <div class="text-center d-flex justify-content-center">
+            <h1 class="text-center my-5 titulo w-100">Galeria de Games</h1>
+        </div>
+        <div class="container mt-5">
+            <div class="row mt-3">
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/csgo.png" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">CS GO</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#csgo">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/battle.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">BattleField1</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#battle">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/crack.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">CrackDown</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <!-- <a href="#" class="btn btn-primary">Sobre</a> -->
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#crack">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/gta.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">GTA</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#gta">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/gtaV.jfif" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">GTA V</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#gtav">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/gtaViceCity.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">GTA VICE CITY</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal"
+                                data-target="#gtaViceCity">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row my-3 ">
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/pes2020.jpeg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">PES 2019</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pes">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/fifa.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">FIFA 2020</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#fifa">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm text-center d-flex justify-content-center">
+                    <div class="card" style="width: 18rem;">
+                        <img src="img/fifaStreet.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">FIFASTREET</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                                of the card's content.</p>
+                            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#fifaStreet">
+                                Sobre
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- CS GO -->
+        <div class="modal fade" id="csgo" tabindex="-1" role="dialog" aria-labelledby="csgo" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">Counter-Strike Global Offensive</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://media.giphy.com/media/3o6fJ8lA3gfUTs3li8/giphy.gif">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Battle -->
+        <div class="modal fade" id="battle" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">Battlefield 1</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://media.giphy.com/media/1gTRQZKftT8bwuZ2N9/giphy.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Crack -->
+        <div class="modal fade" id="crack" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">Crackdown</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://media.giphy.com/media/rkib3KQOSw2bK/giphy.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- GTA -->
+        <div class="modal fade" id="gta" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">GTA</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://thumbs.gfycat.com/MajesticRemoteLark-small.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- GTA V -->
+        <div class="modal fade" id="gtav" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">GTA V</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="http://31.media.tumblr.com/7ee64adb25070e2c2c9fce3867bf4a99/tumblr_mt2n78Bzkq1rz0u7wo1_400.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- GTA Vice City -->
+        <div class="modal fade" id="gtaViceCity" tabindex="-1" role="dialog" aria-labelledby="battle"
+            aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">GTA VICE CITY</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://media.giphy.com/media/l4EoLHlht9TS25Xna/giphy.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Pes 2019 -->
+        <div class="modal fade" id="pes" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">Pes 2019</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://thumbs.gfycat.com/ImaginaryFlamboyantElkhound-size_restricted.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Fifa 20 -->
+        <div class="modal fade" id="fifa" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">Fifa 20</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://www.fifauteam.com/wp-content/uploads/2018/08/A1609-3.gif" alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- FifaStreet -->
+        <div class="modal fade" id="fifaStreet" tabindex="-1" role="dialog" aria-labelledby="battle" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title text-uppercase" id="exampleModalLabel">FifaStreet</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <img class="img-fluid" src="https://s.yimg.com/os/creatr-uploaded-images/2019-08/750bb9d0-c355-11e9-9df9-3550fb1259ab"
+                            alt="">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+    <script src="sw.js"></script>
+</body>
+
+</html>
